38a9aa111c49e06656a28b31f0ef91b9b043ca1e
Bohdan <trojanj@users.noreply.github.com>
2020-05-10 18:41:40 +0300

    Create createFighterPreview and add style for preview

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -7,6 +7,21 @@ export function createFighterPreview(fighter, position) {
     className: `fighter-preview___root ${positionClassName}`,
   });
 
+  if (fighter) {
+    const fighterImage = createFighterImage(fighter);
+    const name = createElement({tagName: 'p'});
+    const health = createElement({tagName: 'p'});
+    const atack = createElement({tagName: 'p'});
+    const defense = createElement({tagName: 'p'});
+    name.innerText = `Name: ${fighter.name}`;
+    health.innerText = `Health: ${fighter.health}`;
+    atack.innerText = `Attack: ${fighter.attack}`;
+    defense.innerText = `Defense: ${fighter.defense}`;
+
+    fighterElement.append(fighterImage, name, health, atack, defense);
+  }
+  
+
   // todo: show fighter info (image, name, health, etc.)
 
   return fighterElement;
