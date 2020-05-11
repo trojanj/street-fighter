006f4ebbfa62943a30b06641c94d2c83d305998c
Bohdan <trojanj@users.noreply.github.com>
2020-05-11 20:35:39 +0300

    create showWinnerModal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,8 @@
+import { showModal } from './modal';
+
 export function showWinnerModal(fighter) {
-  // call showModal function 
+  showModal({
+    title: `${fighter.name} won!!!`,
+    bodyElement: ''
+  })
 }
